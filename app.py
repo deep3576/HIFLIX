96dd91ff53748f133ca9434de359923a27417a42
inderdeep <deep3576@gmail.com>
2024-06-20 12:42:57 -0400

    update_windows_link E:/MoviesDatabase

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify, send_from_directory, render_template
+import os
+
+app = Flask(__name__, static_folder='static', template_folder='templates')
+# only playable files no folders.
+MOVIES_FOLDER = 'E:/MoviesDatabase/'
+THUMBNAILS_FOLDER = 'static/thumbnails'
+
+# Endpoint to get list of movies
+@app.route('/api/movies', methods=['GET'])
+def get_movies():
+    movies = os.listdir(MOVIES_FOLDER)
+    return jsonify(movies)
+
+# Endpoint to stream a movie
+@app.route('/movies/<movie_name>', methods=['GET'])
+def stream_movie(movie_name):
+    return send_from_directory(MOVIES_FOLDER, movie_name)
+
+# Endpoint to serve the main HTML file
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Endpoint to serve thumbnails
+@app.route('/thumbnails/<thumbnail_name>', methods=['GET'])
+def get_thumbnail(thumbnail_name):
+    return send_from_directory(THUMBNAILS_FOLDER, thumbnail_name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- static/app.js
+++ static/app.js
@@ -1,12 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('/movies')
+    fetch('/api/movies')
         .then(response => response.json())
         .then(movies => {
             const moviesList = document.getElementById('movies-list');
             movies.forEach(movie => {
                 const movieElement = document.createElement('div');
                 movieElement.className = 'movie';
-                movieElement.innerHTML = `<a href="/movies/${movie}" target="_blank">${movie}</a>`;
+
+                // Assuming the thumbnail image has the same name as the movie file but with .jpg extension
+                const thumbnailPath = `/thumbnails/${movie}.jpg`;
+
+                movieElement.innerHTML = `
+                    <img src="${thumbnailPath}" alt="${movie}">
+                    <a href="/movies/${movie}" target="_blank">${movie}</a>
+                `;
+
                 moviesList.appendChild(movieElement);
             });
         });
